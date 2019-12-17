9ecfd11b975814af88215029c71ee182a20ebe26
Jacob Penney <jpenney@Jacobs-MBP.attlocal.net>
2019-12-16 20:26:41 -0800

    init

---
+++ LiquidCrypto.js
@@ -0,0 +1,68 @@
+const LiquidCrypto = async (keypair) => {
+	const generateKeys = () => crypto.subtle.generateKey(
+	    {
+	        name: "ECDH",
+	        namedCurve: "P-256", 
+	    },
+	    true, 
+	    ["deriveKey"] 
+	)
+
+	const liquidState = {
+		keypair: keypair || await generateKeys()
+	}
+
+	const deriveKey = (publicKey) => crypto.subtle.deriveKey(
+	    {
+	        name: "ECDH",
+	        namedCurve: "P-256",
+	        public: publicKey, 
+	    },
+	    liquidState.keypair.privateKey,
+	    { 
+	        name: "AES-GCM", 
+	        length: 256, 
+	    },
+	    false, 
+	    ["encrypt", "decrypt"] 
+	)
+
+	const encrypt = async (data, key) => {
+		const iv = crypto.getRandomValues(new Uint8Array(12))
+		const dataBuffer = new TextEncoder().encode(data).buffer
+		const encrypted = await crypto.subtle.encrypt(
+			{
+				name: 'AES-GCM',
+				iv
+			},
+			key,
+			dataBuffer
+		)
+
+		const ivEncryptedBlob = new Blob([iv, encrypted])
+		const ivEncryptedBuffer = await (await new Response(ivEncryptedBlob)).arrayBuffer()
+		return ivEncryptedBuffer
+	}
+
+	const decrypt = async (data, key) => {
+		const iv = data.slice(0, 12)
+		const encData = data.slice(12)
+		return new TextDecoder().decode(await crypto.subtle.decrypt(
+			{
+				name: "AES-GCM",
+				iv
+			},
+			key,
+			encData
+		))
+	}
+
+	return {
+		publicKey: liquidState.keypair.publicKey,
+		deriveKey,
+		encrypt,
+		decrypt
+	}
+}
+
+export default LiquidCrypto
---
+++ test/src/index.js
@@ -0,0 +1,27 @@
+// local import test: 
+import LiquidCrypto from '../../LiquidCrypto.js'
+const init = async () => {
+
+	// instantiate keypairs
+	const liquidCrypto = await LiquidCrypto()
+	const liquidCrypto2 = await LiquidCrypto()
+
+	// derive symmetric encryption key
+	const secretKey2 = await liquidCrypto2.deriveKey(liquidCrypto.publicKey)
+
+	const secret = 'secret data'
+
+	console.log('secret: ', secret)
+
+	const encodedSecret = await liquidCrypto2.encrypt(secret, secretKey2)
+
+	console.log('encrypted secret: ', encodedSecret)
+
+	const secretKey = await liquidCrypto.deriveKey(liquidCrypto2.publicKey)
+
+	const decodedSecret = await liquidCrypto.decrypt(encodedSecret, secretKey)
+
+	console.log('decodedSecret: ', decodedSecret)
+}
+
+init()
